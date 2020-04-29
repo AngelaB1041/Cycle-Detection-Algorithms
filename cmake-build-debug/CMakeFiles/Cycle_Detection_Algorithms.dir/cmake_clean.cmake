33f2035607a55621955d9a7a333e0788a1e4fa62
Angela Barsallo <abarsallo@smu.edu>
2020-04-29 17:51:45 -0500

    Recreated project in CLion
    Have the correct template to run on now

---
+++ cmake-build-debug/CMakeFiles/Cycle_Detection_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cycle_Detection_Algorithms.dir/main.cpp.obj"
+  "Cycle_Detection_Algorithms.exe"
+  "Cycle_Detection_Algorithms.exe.manifest"
+  "Cycle_Detection_Algorithms.pdb"
+  "libCycle_Detection_Algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cycle_Detection_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Cycle_Detection_Algorithms.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cycle_Detection_Algorithms.
+# This may be replaced when dependencies are built.
